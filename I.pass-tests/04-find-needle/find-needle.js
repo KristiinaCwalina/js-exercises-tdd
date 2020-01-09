abb6a263d17c67fae437466390d6d1ef84bea36e
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-09 18:55:58 +0100

    searchwords

--- I.pass-tests/04-find-needle/find-needle.js
+++ I.pass-tests/04-find-needle/find-needle.js
@@ -1,3 +1,11 @@
-function findNeedle(words) {}
+function findNeedle(words, searchedWord) {
+  for (let i = 0; i < words.length; i++) {
+    const element = words[i];
+    console.log(element);
+    if (element == searchedWord) {
+      return i;
+    }
+  }
+}
 
 module.exports = findNeedle;

f67ee8ccde1c743a75d1739c1bce7976e535956a
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-09 17:32:09 +0100

    add-one function implementation

--- I.pass-tests/01-add-one/add-one.js
+++ I.pass-tests/01-add-one/add-one.js
@@ -1,1 +1,8 @@
-module.exports = function(numbers) {};
+module.exports = function addOne(myArray) {
+  let result = [];
+
+  for (let i = 0; i < myArray.length; i++) {
+    result[i] = myArray[i] + 1;
+  }
+  return result;
+};

40d23822d8988c3674acbb467f1ac5d0b32f633e
KristiinaCwalina <kristiina.cwalina@gmail.com>
2020-01-11 12:01:58 +0100

    greet people

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,10 @@
+let greetPeople = require("./greet-people");
 test("print list of names prefixed with Hello", function() {
   // Arrange
+  let people = ["Jose", "Maria", "Eduard", "Laura"];
+  let expectedResult = "Hello JoseMariaEduardLaura";
+  let result = greetPeople(people);
+  expect(result).toBe(expectedResult);
   // Act
   // Assert
 });
